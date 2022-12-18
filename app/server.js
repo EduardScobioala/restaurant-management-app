5ae7f107b5d2588cea10fbcf7dc5e9d3f62e596a
Eduard <eduard@mail.com>
2022-12-18 18:39:00 +0200

    add initial setup

---
+++ app/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
+const indexRouter = require("./routes/index");
+// const clientsRouter = require("./routes/customer");
+// const productsRouter = require("./routes/product");
+// const transactionsRouter = require("./routes/transaction");
+
+// layout/views setup
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded());
+
+// connect to db
+const DBConnection = require("./models/DBConnection.class");
+const db = DBConnection.getInstance();
+db.connect((error) => {
+    if (error) console.log(`Error: ${error.message}`)
+    else console.log("Connected to MySQL Server");
+});
+
+// main router
+app.use("/app", indexRouter);
+// app.use("/customers", clientsRouter);
+// app.use("/products", productsRouter);
+// app.use("/transactions", transactionsRouter);
+
+app.listen(8080, () => {
+    console.log("Listening on port 8080...");
+});
